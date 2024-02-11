2ea4b49bf5ddd1ea08c858fc0c764091d368e9af
Арсений Стефанов <sirsenya@Arsenijs-MacBook-Pro.local>
2024-02-11 19:50:18 +0300

    feat: implement snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,25 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
-
+import { generateLinkedList } from './index';
+jest.setTimeout(30000);
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const linkedList = generateLinkedList(['first', null]);
+    expect(linkedList).toStrictEqual({
+      value: 'first',
+      next: {
+        value: null,
+        next: {
+          next: null,
+          value: null,
+        },
+      },
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const linkedList = generateLinkedList([{ a: 'b' }, 'somesh', 5, false]);
+    expect(linkedList).toMatchSnapshot();
   });
 });
